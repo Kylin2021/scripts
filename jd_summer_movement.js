de1823963d0f2619573560fe0dcd5c2df12c48dc
Kylin <q657019212@gmail.com>
2021-07-08 23:28:49 +0800

    Update jd_summer_movement.js

--- jd_summer_movement.js
+++ jd_summer_movement.js
@@ -23,13 +23,13 @@ if ($.isNode() && process.env.summer_movement_joinjoinjoinhui) {
   summer_movement_joinjoinjoinhui = process.env.summer_movement_joinjoinjoinhui;
 }
 
-let ShHelpFlag = true;//是否SH助力  true 助力，false 不助力
+let ShHelpFlag = false;//是否SH助力  true 助力，false 不助力
 if ($.isNode() && process.env.ShHelpFlag) {
   ShHelpFlag = process.env.ShHelpFlag;
 }
 
 
-const ShHelpAuthorFlag = true;//是否助力作者SH  true 助力，false 不助力
+const ShHelpAuthorFlag = false;//是否助力作者SH  true 助力，false 不助力
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [];
 $.cookie = '';
